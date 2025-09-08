65f40e2423dcaa49e07fc283cfbb0a81aedcc6c6
lemon_juice_1 <1582017385@qq.com>
2025-09-08 23:21:24 +0800

    添加指数退避机制

--- src/main/java/io/lemonjuice/flandre_bot/network/WSReconnect.java
+++ src/main/java/io/lemonjuice/flandre_bot/network/WSReconnect.java
@@ -5,12 +5,14 @@ import lombok.extern.log4j.Log4j2;
 
 @Log4j2
 public class WSReconnect implements Runnable {
+    private int waitTime = 5000;
+
     @Override
     public void run() {
         int retry_count = 0;
         while(true) {
             try {
-                Thread.sleep(10000);
+                Thread.sleep(this.waitTime);
             } catch (InterruptedException e) {
                 break;
             }
@@ -19,6 +21,12 @@ public class WSReconnect implements Runnable {
             if (WSClientCore.connect(BotBasicConfig.WS_URL.get(), BotBasicConfig.WS_TOKEN.get())) {
                 log.info("重连成功! ");
                 break;
+            } else {
+                log.info("重连失败，将在{}秒后重试", this.waitTime / 1000);
+            }
+
+            if(this.waitTime < 160000) {
+                this.waitTime *= 2;
             }
         }
     }
