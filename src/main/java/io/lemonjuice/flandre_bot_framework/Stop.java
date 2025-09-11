c500114087fe250a0162876e8e12f3f6fc74ac1a
lemon_juice_1 <1582017385@qq.com>
2025-09-11 21:47:59 +0800

    记录日志

--- src/main/java/io/lemonjuice/flandre_bot_framework/Stop.java
+++ src/main/java/io/lemonjuice/flandre_bot_framework/Stop.java
@@ -3,10 +3,13 @@ package io.lemonjuice.flandre_bot_framework;
 import io.lemonjuice.flandre_bot_framework.event.BotEventBus;
 import io.lemonjuice.flandre_bot_framework.event.meta.BotStopEvent;
 import io.lemonjuice.flandre_bot_framework.network.WSClientCore;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class Stop implements Runnable {
     @Override
     public void run() {
+        log.info("正在停止应用...");
         BotEventBus.post(new BotStopEvent());
         WSClientCore.close();
     }
