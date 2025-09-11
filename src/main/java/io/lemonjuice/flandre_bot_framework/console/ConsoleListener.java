fe022d37c6c8b219daffd6df50a778b864bd4d99
lemon_juice_1 <1582017385@qq.com>
2025-09-12 01:52:05 +0800

    改用BufferedReader

--- src/main/java/io/lemonjuice/flandre_bot_framework/console/ConsoleListener.java
+++ src/main/java/io/lemonjuice/flandre_bot_framework/console/ConsoleListener.java
@@ -1,27 +1,42 @@
 package io.lemonjuice.flandre_bot_framework.console;
 
+import lombok.extern.log4j.Log4j2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
 import java.util.function.Function;
 
+@Log4j2
 public class ConsoleListener implements Runnable {
-    private final Scanner scanner = new Scanner(System.in);
+    private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     @Override
     public void run() {
         System.out.println("控制台命令系统已启动, 使用 'help' 或 '?' 查看命令列表");
 
         while(true) {
-            String command = scanner.nextLine();
-            String[] commandArray = Arrays.stream(command.split(" ")).filter(str -> !str.isEmpty()).toArray(String[]::new);
-            String commandBody = commandArray[0];
-            if(ConsoleCommandLookup.CONSOLE_COMMANDS.containsKey(commandBody)) {
-                String[] args = Arrays.copyOfRange(commandArray, 1, commandArray.length);
-                Function<String[], ConsoleCommandRunner> provider = ConsoleCommandLookup.CONSOLE_COMMANDS.get(commandBody);
-                ConsoleCommandRunner runner = provider.apply(args);
-                runner.apply();
-            } else {
-                System.out.println("未知命令, 使用 'help' 或 '?' 查看命令列表");
+            try {
+                String command = reader.readLine();
+                String[] commandArray = Arrays.stream(command.split(" ")).filter(str -> !str.isEmpty()).toArray(String[]::new);
+                String commandBody = commandArray[0];
+                if (ConsoleCommandLookup.CONSOLE_COMMANDS.containsKey(commandBody)) {
+                    String[] args = Arrays.copyOfRange(commandArray, 1, commandArray.length);
+                    Function<String[], ConsoleCommandRunner> provider = ConsoleCommandLookup.CONSOLE_COMMANDS.get(commandBody);
+                    ConsoleCommandRunner runner = provider.apply(args);
+                    runner.apply();
+                } else {
+                    System.out.println("未知命令, 使用 'help' 或 '?' 查看命令列表");
+                }
+            } catch (IOException e) {
+                log.error("获取命令输入失败！", e);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
             }
         }
     }
