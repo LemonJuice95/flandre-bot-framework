ace083f41b7a3fd2d2e93b03a3f7314f546038d6
lemon_juice_1 <1582017385@qq.com>
2025-09-12 02:33:46 +0800

    使用JLine

--- src/main/java/io/lemonjuice/flandre_bot_framework/console/ConsoleListener.java
+++ src/main/java/io/lemonjuice/flandre_bot_framework/console/ConsoleListener.java
@@ -1,23 +1,24 @@
 package io.lemonjuice.flandre_bot_framework.console;
 
 import lombok.extern.log4j.Log4j2;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.function.Function;
 
 @Log4j2
 public class ConsoleListener implements Runnable {
-    private final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
     @Override
     public void run() {
-        System.out.println("控制台命令系统已启动, 使用 'help' 或 '?' 查看命令列表");
+        try (Terminal terminal = TerminalBuilder.builder().system(true).build()) {
+            LineReader reader = LineReaderBuilder.builder().terminal(terminal).build();
+            System.out.println("控制台命令系统已启动, 使用 'help' 或 '?' 查看命令列表");
 
-        while(true) {
-            try {
+            while (true) {
                 String command = reader.readLine();
                 String[] commandArray = Arrays.stream(command.split(" ")).filter(str -> !str.isEmpty()).toArray(String[]::new);
                 String commandBody = commandArray[0];
@@ -29,15 +30,9 @@ public class ConsoleListener implements Runnable {
                 } else {
                     System.out.println("未知命令, 使用 'help' 或 '?' 查看命令列表");
                 }
-            } catch (IOException e) {
-                log.error("获取命令输入失败！", e);
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException ex) {
-                    Thread.currentThread().interrupt();
-                    break;
-                }
             }
+        } catch (IOException e) {
+            log.error("控制台命令系统启动失败！", e);
         }
     }
 }
