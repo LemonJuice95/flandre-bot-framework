395090f239095193e536624d951abb5c3273e6f2
lemon_juice_1 <1582017385@qq.com>
2025-09-16 22:41:22 +0800

    数据对结构

---
+++ src/main/java/io/lemonjuice/flandre_bot_framework/utils/data/Pair.java
@@ -0,0 +1,18 @@
+package io.lemonjuice.flandre_bot_framework.utils.data;
+
+import lombok.Getter;
+
+@Getter
+public class Pair<F, S> {
+    private final F first;
+    private final S second;
+
+    private Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <F, S> Pair<F, S> of(F first, S second) {
+        return new Pair<>(first, second);
+    }
+}
