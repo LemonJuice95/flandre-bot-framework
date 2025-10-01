bdae4179f8489669c118d335d9485b89e2dffbf0
lemon_juice_1 <1582017385@qq.com>
2025-10-01 23:09:29 +0800

    重写equals和hashCode方法

--- src/main/java/io/lemonjuice/flandre_bot_framework/utils/data/Pair.java
+++ src/main/java/io/lemonjuice/flandre_bot_framework/utils/data/Pair.java
@@ -2,6 +2,8 @@ package io.lemonjuice.flandre_bot_framework.utils.data;
 
 import lombok.Getter;
 
+import java.util.Objects;
+
 @Getter
 public class Pair<F, S> {
     private final F first;
@@ -14,5 +16,19 @@ public class Pair<F, S> {
 
     public static <F, S> Pair<F, S> of(F first, S second) {
         return new Pair<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj instanceof Pair<?,?> pair) {
+            return Objects.equals(this.first, pair.getFirst()) &&
+                    Objects.equals(this.second, pair.getSecond());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.first, this.second);
     }
 }
