f828fd6b54fc4b4fa9988942ffd1df421a83bee2
lemon_juice_1 <1582017385@qq.com>
2025-10-05 00:35:50 +0800

    我也不知道为什么要改

--- src/main/java/io/lemonjuice/flandre_bot_framework/network/NetworkMode.java
+++ src/main/java/io/lemonjuice/flandre_bot_framework/network/NetworkMode.java
@@ -7,6 +7,7 @@ public enum NetworkMode {
     WS_CLIENT("Websocket正向连接"),
     WS_SERVER("Websocket反向连接"),
     HTTP_CLIENT("Http正向连接"),
+    HTTP_SERVER("Http反向连接"),
     UNKNOWN("未知");
 
     private final String name;
