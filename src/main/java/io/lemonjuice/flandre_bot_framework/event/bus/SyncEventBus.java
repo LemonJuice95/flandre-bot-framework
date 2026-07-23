658eb4af889bb03a50b5c6dd311b77deaf8a80dc
lemon_juice_1 <1582017385@qq.com>
2026-07-23 15:01:32 +0800

    使用方法句柄优化性能

--- src/main/java/io/lemonjuice/flandre_bot_framework/event/bus/SyncEventBus.java
+++ src/main/java/io/lemonjuice/flandre_bot_framework/event/bus/SyncEventBus.java
@@ -5,6 +5,8 @@ import io.lemonjuice.flandre_bot_framework.event.annotation.SubscribeEvent;
 import io.lemonjuice.flandre_bot_framework.utils.data.Pair;
 import lombok.extern.log4j.Log4j2;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
 import java.lang.reflect.Method;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
@@ -12,7 +14,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 @Log4j2
 public class SyncEventBus {
-    private final ConcurrentHashMap<Class<?>, CopyOnWriteArrayList<Pair<Object, Method>>> subscribers = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<Class<?>, CopyOnWriteArrayList<Subscriber>> subscribers = new ConcurrentHashMap<>();
 
     public void register(Object object) {
         Method[] methods = object.getClass().getMethods();
@@ -21,9 +23,13 @@ public class SyncEventBus {
                 Class<?>[] params = method.getParameterTypes();
                 if(params.length == 1 && Event.class.isAssignableFrom(params[0])) {
                     Class<?> eventClass = params[0];
-                    CopyOnWriteArrayList<Pair<Object, Method>> list = this.getOrCreateList(eventClass);
+                    CopyOnWriteArrayList<Subscriber> list = this.getOrCreateList(eventClass);
                     method.setAccessible(true);
-                    list.add(Pair.of(object, method));
+                    try {
+                        list.add(new Subscriber(object, method));
+                    } catch (IllegalAccessException e) {
+                        log.warn("{}中的{}方法无法从外部访问，无法注册", object.getClass().getSimpleName(), method.getName());
+                    }
                 } else {
                     log.warn("{}中的{}方法不符合订阅者方法的条件，无法注册", object.getClass().getSimpleName(), method.getName());
                 }
@@ -34,14 +40,13 @@ public class SyncEventBus {
     public void post(Event event) {
         Class<?> clazz = event.getClass();
         while (clazz != Object.class) {
-            List<Pair<Object, Method>> subscriberMethods = this.subscribers.get(clazz);
-            if(subscriberMethods != null) {
-                for(Pair<Object, Method> pair : subscriberMethods) {
-                    Method method = pair.getSecond();
+            List<Subscriber> subscribers = this.subscribers.get(clazz);
+            if(subscribers != null) {
+                for(Subscriber subscriber : subscribers) {
                     try {
-                        method.invoke(pair.getFirst(), event);
-                    } catch (Exception e) {
-                        log.error("无法执行订阅者方法\"{}.{}\"", pair.getFirst().getClass().getSimpleName(), method.getName(), e);
+                        subscriber.post(event);
+                    } catch (Throwable e) {
+                        log.error("无法执行订阅者方法\"{}.{}\"", subscriber.target.getClass().getSimpleName(), subscriber.method.getName(), e);
                     }
                 }
             }
@@ -51,11 +56,29 @@ public class SyncEventBus {
 
     public void unregister(Object object) {
         this.subscribers.values().forEach((list) -> {
-            list.removeIf(pair -> pair.getFirst().equals(object));
+            list.removeIf(s -> s.target.equals(object));
         });
     }
 
-    private CopyOnWriteArrayList<Pair<Object, Method>> getOrCreateList(Class<?> clazz) {
+    private CopyOnWriteArrayList<Subscriber> getOrCreateList(Class<?> clazz) {
         return this.subscribers.computeIfAbsent(clazz, k -> new CopyOnWriteArrayList<>());
+    }
+
+    private static class Subscriber {
+        public final Object target;
+        public final Method method;
+        public final MethodHandle methodHandle;
+
+        public Subscriber(Object target, Method method) throws IllegalAccessException {
+            this.target = target;
+            this.method = method;
+
+            MethodHandles.Lookup lookup = MethodHandles.lookup();
+            this.methodHandle = lookup.unreflect(method).bindTo(target);
+        }
+
+        public void post(Event event) throws Throwable {
+            this.methodHandle.invoke(event);
+        }
     }
 }
